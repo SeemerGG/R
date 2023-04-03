cba88f4960fb5f6edf2f3eb3f0039d4b3f794118
SeemerGG <92618851+SeemerGG@users.noreply.github.com>
2023-04-03 14:16:07 +0300

    Update lab3.R

--- Lab3/lab3.R
+++ Lab3/lab3.R
@@ -2,11 +2,29 @@
 #library(xlsx)
 #dataXml <- read.xlsx("answers.xlsx")
 #install.packages("dplyr")
-library(dplyr)
-dataCsv <- read.csv("answers.csv")
-columnName <- colnames(dataCsv)
+
+#library(dplyr)
+dataCsv <- read.csv("answers.csv", encoding = "UTF-8") #создание фрейма
+
+print(summary(dataCsv)) #производим дискриптивный анализ
 boxplot(dataCsv[1:10, 3:ncol(dataCsv)], col = rainbow(10))# строим коробку с усами определяем медиану, нижний ус, первый квартиль, третитий квартиль, и верхний ус
-hist(dataCsv$Бегущий.по.лезвию.2049, breaks = 10) #строим гистограммы определяем моды по ним
+
+sortedData <- sort(sapply(dataCsv[1:10, 3:ncol(dataCsv)], mean, na.rm = TRUE), decreasing = TRUE)
+hist(sortedData,main = "Mean",xlab="Значения",ylab="Количество", col="#000000") #сортировка по выбраному признаку
+
+dataDrive <- subset(dataCsv, Драйв %in% c(8, 9, 10))
+print(dataDrive) # сформировал отдельный набор данных по признаку (Оценка для фильма Драйв от 8 до 10)
+
+print(dim(dataDrive)) #подсщитал размерность новой таблицы 
+print(summary(dataDrive)) #изучение новых данных 
+
+sortedDrive <- sort(sapply(dataDrive[1:10, 3:ncol(dataDrive)], mean, na.rm = TRUE), decreasing = TRUE)
+hist(sortedDrive,main = "Mean",xlab="Значения",ylab="Количество", col="#000000")
+boxplot(dataDrive[1:10, 3:ncol(dataDrive)], col = rainbow(10))
+#Конец лабы
+
+
+hist(dataCsv$Бегущий.по.лезвию.2049, breaks = 10) #строим гистограммы определяем моды по моды (не надо это делать на защите не пригодилось)
 hist(dataCsv$Драйв, breaks = 10)
 hist(dataCsv$Игра.на.понижение, breaks = 10)
 hist(dataCsv$Ла.Ла.Ленд, breaks = 10)
@@ -16,4 +34,5 @@ hist(dataCsv$Место.под.соснами, breaks = 10)
 hist(dataCsv$Эта.дурацкая.любовь, breaks = 10)
 hist(dataCsv$Фанатик, breaks = 10)
 hist(dataCsv$Ларс.и.настоящая.девушка, breaks = 10)
-arrange(dataCsv[1:10, 3:ncol(dataCsv)], mean(dataCsv$Бегущий.по.лезвию.2049))
+
+dataCsv <- sorbox2
